bea9322663d5d08fea4c6d506a288b6483550617
Cedric-SAIT <Cedric.Repsina@edu.sait.ca>
2023-10-19 17:49:04 -0600

    adding the vowel count (part 2)

--- count.py
+++ count.py
@@ -2,3 +2,10 @@
 # Read out the information off of sample.ini file 
 sample_ini = open("sample.ini")
 information = sample_ini.read()
+
+# counting the vowels within sample.ini file
+counter = 0
+for vowels in information:
+    # list of the vowels
+    if vowels in ["a","e","i","o","u","y"]:
+        counter += 1 
