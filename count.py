e0f580c4719babc3cf3c412a7a77fa5f01499ea3
Cedric-SAIT <Cedric.Repsina@edu.sait.ca>
2023-10-19 17:42:27 -0600

    part 2

--- count.py
+++ count.py
@@ -1,5 +1,4 @@
+# Finding and opening the sample.ini file
+# Read out the information off of sample.ini file 
 sample_ini = open("sample.ini")
 information = sample_ini.read()
-print(type(information))
-print(information)
-
