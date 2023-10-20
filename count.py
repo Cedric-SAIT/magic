b47981699b5cd3d85b42a22254607bab7d5dabae
Cedric-SAIT <Cedric.Repsina@edu.sait.ca>
2023-10-19 18:22:36 -0600

    creating a .txt file to write sum of vowel counter

--- count.py
+++ count.py
@@ -9,3 +9,14 @@ for vowels in information:
     # list of the vowels
     if vowels in ["a","e","i","o","u","y"]:
         counter += 1 
+
+# producing file with vowel counts
+information = open("counts.txt", "w")
+# writing the vowel count onto the .txt file
+vowels = information.write(f"The total amount of vowels is {counter}")
+# opens file to display information 
+information = open("counts.txt", "r")
+vowels = information.read()
+information.close()
+
+
